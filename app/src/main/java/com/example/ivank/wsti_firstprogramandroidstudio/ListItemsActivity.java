c78cf55e8c8c93eb30654449be33cbd961e773d0
Karba <ivankarbashevskyi@gmail.com>
2018-11-07 21:38:06 +0100

    H: This version have listView with file reader and file saved, this version need on 08.11.2018

--- app/src/main/java/com/example/ivank/wsti_firstprogramandroidstudio/ListItemsActivity.java
+++ app/src/main/java/com/example/ivank/wsti_firstprogramandroidstudio/ListItemsActivity.java
@@ -16,13 +16,28 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ListItemsActivity extends AppCompatActivity {
 
     private ListView listView;
-    private String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand"};
+    private ArrayList<String> list;
+    private ArrayAdapter<String> arrayAdapter;
+//    private String[] countryList = {};
+
+    List<String> countryList = new ArrayList<>();
+
+    final String LOG_TAG = "myLogs";
+    final String FILENAME = "karbashevskyi";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,10 +51,14 @@ public class ListItemsActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_list);
+        getAllInformation();
 
         listView = findViewById(R.id.listView);
-        final ArrayList<String> list = new ArrayList<String>(Arrays.asList(countryList));
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_item, R.id.textViewForItem, list);
+
+        list = new ArrayList<String>(countryList);
+
+
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_item, R.id.textViewForItem, list);
         listView.setAdapter(arrayAdapter);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
@@ -62,6 +81,7 @@ public class ListItemsActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         list.add(input.getText().toString());
+                        saveInformation();
                         Toast.makeText(ListItemsActivity.this, "You added new item to list, it`s : " + input.getText().toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -102,6 +122,7 @@ public class ListItemsActivity extends AppCompatActivity {
 
                         list.remove(position);
                         arrayAdapter.notifyDataSetChanged();
+                        saveInformation();
                         Toast.makeText(ListItemsActivity.this, "You delete : " + item, Toast.LENGTH_SHORT).show();
 
 
@@ -116,6 +137,77 @@ public class ListItemsActivity extends AppCompatActivity {
 
     }
 
+    private void saveInformation() {
+        try {
+            // отрываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput(FILENAME, MODE_PRIVATE)));
+            // пишем данные
+            for (int i=0;i<arrayAdapter.getCount();i++){
+                bw.write(arrayAdapter.getItem(i) + ",");
+            }
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан, count: " + arrayAdapter.getCount());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getAllInformation() {
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput(FILENAME)));
+            String str = "";
+            boolean haveData = false;
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
+                String test[] = str.split(",");
+                if (test.length > 0) {
+                    haveData  = true;
+                    Log.d(LOG_TAG, test.length + " - count country saved");
+                    for (int i = 0; i < test.length; i++) {
+                        countryList.add(test[i] + "");
+                    }
+                }
+            }
+            Log.d(LOG_TAG, haveData ? "true" : "false");
+            if (!haveData) {
+                countryList.add("India");
+                countryList.add("China");
+                countryList.add("australia");
+                countryList.add("Portugle");
+                countryList.add("America");
+                countryList.add("NewZealand");
+                countryList.add("China");
+                countryList.add("australia");
+                countryList.add("Portugle");
+                countryList.add("America");
+                countryList.add("NewZealand");
+                countryList.add("China");
+                countryList.add("australia");
+                countryList.add("Portugle");
+                countryList.add("America");
+                countryList.add("NewZealand");
+                countryList.add("China");
+                countryList.add("australia");
+                countryList.add("Portugle");
+                countryList.add("America");
+                countryList.add("NewZealand");
+                countryList.add("Karbashevskyi");
+                saveInformation();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
